7e0f79efc8287d5ed3f97f7177a0feb52cd05ccc
NItro <atadjanovdavronbek863@gmail.com>
2025-02-04 23:22:05 +0500

    webcoket and django chanells configuration

--- a_core/settings.py
+++ a_core/settings.py
@@ -21,6 +21,7 @@ CSRF_TRUSTED_ORIGINS = [ 'https://*' ]
 # Application definition
 
 INSTALLED_APPS = [
+    'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,8 +77,8 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'a_core.wsgi.application'
-
+# WSGI_APPLICATION = 'a_core.wsgi.application'
+ASGI_APPLICATION = 'a_core.asgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
