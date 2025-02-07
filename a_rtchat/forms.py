8895dda629b3b469cc4eb7d6eca4761c723ca8fa
NItro <atadjanovdavronbek863@gmail.com>
2025-02-07 20:45:49 +0500

    groupchat(create, delete, admin role)

--- a_rtchat/forms.py
+++ a_rtchat/forms.py
@@ -1,6 +1,7 @@
 from django.forms import ModelForm
 from django.forms import TextInput
-from .models import GroupMessage
+from .models import GroupMessage, ChatGroup
+
 
 class ChatMessageCreateForm(ModelForm):
     class Meta:
@@ -8,4 +9,29 @@ class ChatMessageCreateForm(ModelForm):
         fields = ['body']
         widgets = {
             'body': TextInput(attrs={'placeholder':'Add message...', 'class': 'p-4 text-black', 'maxlength':'300', 'autofocus': True}),
+        }
+
+class NewGroupForm(ModelForm):
+    class Meta:
+        model = ChatGroup
+        fields = ['groupchat_name']
+        widgets = {
+            'groupchat_name': TextInput(attrs={
+                'placeholder': 'Add name ...',
+                'class': 'p-4 text-black',
+                'maxlength': '300',
+                'autofocus': True,
+            }),
+        }
+
+
+class ChatRoomEditForm(ModelForm):
+    class Meta:
+        model = ChatGroup
+        fields = ['groupchat_name']
+        widgets = {
+            'groupchat_name': TextInput(attrs={
+                'class': 'p-4 text-xl font-bold mb-4',
+                'maxlengh': '300',
+            }),
         }
--- a_rtchat/urls.py
+++ a_rtchat/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from .views import get_or_create_chatroom
 
-from a_rtchat.views import chat_view
+from a_rtchat.views import chat_view, create_groupchat, chatroom_edit_view, chatroom_delete_view, chatroom_leave_view
 
 urlpatterns = [
     path('', chat_view, name="home"),
     path('chat/<username>', get_or_create_chatroom, name="start-chat"),
-    path('chat/room/<chatroom_name>', chat_view, name="chatroom")
-]
+    path('chat/room/<str:chatroom_name>', chat_view, name="chatroom"),
+    path('chat/new_groupchat/', create_groupchat, name="new-groupchat"),
+    path('chat/edit/<chatroom_name>', chatroom_edit_view, name="edit-chatroom"),
+    path('chat/delete/<chatroom_name>', chatroom_delete_view, name="chatroom-delete"),
+    path('chat/leave/<chatroom_name>', chatroom_leave_view, name="chatroom-leave")
+ ]
